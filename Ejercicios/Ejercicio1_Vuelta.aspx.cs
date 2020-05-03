f06914a9949bfe33288bb78d9f02cf2eb2a98e61
Cristian Nieto <nieto.cristian.alberto@gmail.com>
2020-05-03 19:06:17 -0300

    first commit introducing Ejercicio 1

---
+++ Ejercicios/Ejercicio1_Vuelta.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejercicios
+{
+    public partial class Ejercicio1_Vuelta : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(Calculate(Request.Form.Get("txtApellido"), Request.Form.Get("txtEdad")));
+        }
+
+        public string Calculate(string apellido, string edad)
+        {
+            try
+            {
+                int edadEnMeses = Convert.ToInt16(edad) * 12;
+                return(apellido + " su edad en meses es: " + edadEnMeses);
+            }
+            catch (Exception ex)
+            {
+                return("Ocurrio un error al procesar los datos enviados. Por favor intente nuevamente");
+            }
+        }
+    }
+}
