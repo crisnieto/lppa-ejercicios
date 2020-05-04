91f758940583837b32451fddf8b07b685172fcf2
Cristian Nieto <nieto.cristian.alberto@gmail.com>
2020-05-03 22:58:30 -0300

    Fixing UnitTest project

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicios;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Ejercicio1VueltaTest
+    {
+        [TestMethod]
+        public void Load()
+        {
+            //Arrange
+            Ejercicio1_Vuelta ejercicio = new Ejercicio1_Vuelta();
+
+            //Act
+            string result = ejercicio.Calculate("Nieto", "27");
+
+            //Assert
+            Assert.AreEqual("Nieto su edad en meses es: 324", result);
+        }
+    }
+}
